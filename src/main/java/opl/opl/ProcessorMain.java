87fac7231265b72a11eba8c95adc59b3c2eddb2f
VaubanParty <yassine.badache@gmail.com>
2015-11-12 21:24:04 +0100

    Ajout commentaires

--- src/main/java/opl/opl/ProcessorMain.java
+++ src/main/java/opl/opl/ProcessorMain.java
@@ -12,12 +12,13 @@ import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.visitor.filter.TypeFilter;
 
 /**
- * Class that create a call graph
+ * Class that create a call graph from a source file executed with Spoon
  *
- * @author sais
+ * @author sais, badache
  *
  */
 public class ProcessorMain extends AbstractProcessor<CtMethod> {
+	// Global graph for the whole execution
 	static Graph graph;
 	/*
 	 * (non-Javadoc)
@@ -26,27 +27,36 @@ public class ProcessorMain extends AbstractProcessor<CtMethod> {
 	 * spoon.processing.Processor#process(spoon.reflect.declaration.CtElement)
 	 */
 	public void process(CtMethod arg0) {
+		// Every elements which are function calls are registered here
 		List<CtInvocation> elements = arg0.getElements(new TypeFilter(CtInvocation.class));
 
+		// If we have at least one element
 		if (elements != null) {
+			// For each element of the list
 			for (CtInvocation inv : elements) {
+				// Getting its name
 				final String current = inv.getExecutable().getSimpleName();
 				
+				// If it is not a native function (e.g println, error ...)
 				if (inv.getExecutable().getDeclaration() != null) {
+					// If the corresponding node doesn't exist, we create it
 					if (graph.getNode(inv.getExecutable().getSimpleName()) == null) {
 						final Node n = graph.addNode(current);
 						n.addAttribute("ui.label", current);
 					}
 					
-					System.out.println("fct " + current + " parent " + inv.getParent(CtMethod.class).getSimpleName());
-					
+					// If the current method call has a parent (e.g main is a parent of a function called inside it)
 					if (inv.getParent(CtMethod.class).getSimpleName() != null) {
+						// Getting the parent's name
 						final String parent = inv.getParent(CtMethod.class).getSimpleName();
 						
+						// If the parent does not have a dedicated node yet, we create it
 						if (graph.getNode(parent) == null) {
 							final Node n = graph.addNode(parent);
 							n.addAttribute("ui.label", parent);
 						}	
+						
+						// We add an oriented edge between parent and current method call
 						graph.addEdge(String.valueOf(graph.getEdgeCount() + 1), parent, current, true);
 					}
 				}
@@ -62,6 +72,7 @@ public class ProcessorMain extends AbstractProcessor<CtMethod> {
 		final Node n = graph.addNode("main");
 		n.setAttribute("ui.label", "main");
 		
+		// Launching the main process about creating the graph
 		spoon.Launcher.main(new String[] { "-p", "opl.opl.ProcessorMain", "-i", "sources/opl/A.java" });
 
 		graph.display();
